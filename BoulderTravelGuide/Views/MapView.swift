7091b38ded4ed5f1a6d90e0097d522e2fbc2aee0
namratha-p-s <namratha.psom.97@gmail.com>
2023-10-12 10:41:05 -0600

    Added Look Around View for the places

---
+++ BoulderTravelGuide/Views/MapView.swift
@@ -0,0 +1,18 @@
+//
+//  MapView.swift
+//  BoulderTravelGuide
+//
+//  Created by Namratha P Somachudan on 10/12/23.
+//
+
+import SwiftUI
+
+struct MapView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MapView()
+}
